3a1f681585318b8c74036c1fb30b78bafa2a54fe
Jannis Kckeritz (Husel) <koeckeritz@red07.stud.lnm.mw.tum.de>
2024-11-06 15:17:09 +0100

    add multi-level monte-carlo iterator

--- queens/iterators/mlmc_iterator.py
+++ queens/iterators/mlmc_iterator.py
@@ -1,12 +1,14 @@
-import numpy as np
 import logging
 
+import numpy as np
+
 from queens.iterators.iterator import Iterator
-from queens.utils.logger_settings import log_init_args
 from queens.models.simulation_model import SimulationModel
 from queens.schedulers.pool_scheduler import PoolScheduler
+from queens.utils.logger_settings import log_init_args
 
 _logger = logging.getLogger(__name__)
+
 
 class MLMCIterator(Iterator):
     """Multi Level Monte Carlo Iterator for two levels.
@@ -22,9 +24,9 @@ class MLMCIterator(Iterator):
 
     @log_init_args
     def __init__(
-        self, 
-        drivers, 
-        parameters, 
+        self,
+        drivers,
+        parameters,
         global_settings,
         seed,
         num_samples,
@@ -43,8 +45,14 @@ class MLMCIterator(Iterator):
         Raises:
             ValueError: _description_
         """
-        if isinstance(drivers, list) and isinstance(num_samples, list) and len(num_samples) == len(drivers):
-            super().__init__(None, parameters, global_settings) # init parent iterator with no model
+        if (
+            isinstance(drivers, list)
+            and isinstance(num_samples, list)
+            and len(num_samples) == len(drivers)
+        ):
+            super().__init__(
+                None, parameters, global_settings
+            )  # init parent iterator with no model
             self.seed = seed
             self.drivers = drivers
             self.num_samples = num_samples
@@ -69,27 +77,32 @@ class MLMCIterator(Iterator):
         """Generate samples for subsequent MLMC analysis and update model."""
         np.random.seed(self.seed)
         self.samples = self.parameters.draw_samples(sum(self.num_samples))
-    
+
     def core_run(self):
         # check if number of smaples matches the number of samples given
         if len(self.samples) != sum(self.num_samples):
             _logger.warning("number of sampels does not match number of samples given")
 
         # ------------------ level 0 -------------------
-        mean = self.models[0].evaluate(self.samples[:self.num_samples[0]])['result'].mean()
-        pivot = self.num_samples[0] # index to select the right samples out of sample list for each level, gets updated throughout for-loop
+        mean = self.models[0].evaluate(self.samples[: self.num_samples[0]])["result"].mean()
+        pivot = self.num_samples[
+            0
+        ]  # index to select the right samples out of sample list for each level, gets updated throughout for-loop
 
         breakpoint()
 
         # ---------------- levels 1,2,3 ... n ------------------
 
         for i in range(1, len(self.models)):
-            samples = self.samples[pivot:pivot+self.num_samples[i]] # samples index by number of samples on each level and extracted from samples array
+            samples = self.samples[
+                pivot : pivot + self.num_samples[i]
+            ]  # samples index by number of samples on each level and extracted from samples array
 
-            mean += (self.models[i].evaluate(samples)['result'] - self.models[i-1].evaluate(samples)['result']).mean() #Difference between levels is added to the mean
-            pivot+=self.num_samples[i]
-        
-        
+            mean += (
+                self.models[i].evaluate(samples)["result"]
+                - self.models[i - 1].evaluate(samples)["result"]
+            ).mean()  # Difference between levels is added to the mean
+            pivot += self.num_samples[i]
 
         self.output = {
             "result": np.array(mean),
@@ -106,7 +119,7 @@ class MLMCIterator(Iterator):
 - scheduler als Parameter
 - rais error switch if else
 - check num samples decreasing (warning)
-- docstring methoden 
+- docstring methoden
 - samples
 - visualization  mc vs mlmc
 - commiten
