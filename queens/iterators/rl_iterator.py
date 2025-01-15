e4f923858779a7024decb726d2d6430460aeb6b9
Daniel Wolff <d.wolff@unibw.de>
2025-01-15 17:47:57 +0100

    feat: first working draft for RL with a predefined gymnasium environment

--- queens/iterators/rl_iterator.py
+++ queens/iterators/rl_iterator.py
@@ -6,8 +6,9 @@ import time
 from stable_baselines3 import A2C, DDPG, DQN, PPO, SAC, TD3
 
 from queens.iterators.iterator import Iterator
-from queens.models.rl_model import RLModel
+from queens.models.rl_models.rl_model import RLModel
 from queens.utils.logger_settings import log_init_args
+from queens.utils.process_outputs import write_results
 
 _logger = logging.getLogger(__name__)
 
@@ -45,7 +46,7 @@ class RLIterator(Iterator):
     """
 
     @log_init_args
-    def __init__(self, model, parameters, global_settings, agent='PPO', policy='MlpPolicy', total_timesteps=10000, agent_kwargs=None):
+    def __init__(self, model, parameters, global_settings, result_description=None):
         """Initialize an RLIterator.
 
         Args:
@@ -61,39 +62,36 @@ class RLIterator(Iterator):
             total_timesteps (int): The total number of timesteps to train the RL agent.
             agent_kwargs (dict): Additional arguments to pass to the stable-baselines3 agent.
         """
-        super().__init__(model, parameters, global_settings)
-
-        # Check that a valid agent has been provided
-        agent = agent.upper()
-        agent_class = _supported_agents.get(agent, None)
-        if agent_class is None:
-            raise ValueError(f'Unsupported agent: {agent}')
-        
-        # Check that the provided policy is compatible with the chosen agent
-        if policy not in agent_class.policy_aliases.keys():
-            raise ValueError(
-                f'Unsupported policy: `{policy}` for agent `{agent}`!\n'
-                f'Agent {agent} only supports the following policies: '
-                f'{list(agent_class.policy_aliases.keys())}.'
-            )
-        
-        # Check that a valid model has been provided
-        if not issubclass(model, RLModel):
+        # Make sure that a valid model has been provided
+        if not isinstance(model, RLModel):
             raise ValueError(
                 'Unsupported model:\n'
                 '`RLIterator` only supports models that inherit from `RLModel`.'
             )
         
-        # instantiate the stable-baselines3 agent
-        self.agent = agent_class(policy, model, **agent_kwargs)
+        super().__init__(model, parameters, global_settings)
 
-        self.total_timesteps = total_timesteps
+        self.result_description = result_description
 
+    def pre_run(self):
+        """Pre run portion of RLIterator."""
+        _logger.info("Initialize RLIterator.")
 
     def core_run(self):
         """Core run of RLIterator."""
         _logger.info("Welcome to Reinforcement Learning core run.")
         start = time.time()
-        self.agent.learn(self.total_timesteps)
+        # Start the model training
+        self.model.train()
         end = time.time()
         _logger.info("Agent training took %E seconds.", end - start)
+
+    def post_run(self):
+        """Post run portion of RLIterator."""
+        if self.result_description:
+            if self.result_description["write_results"]:
+                _logger.info("Storing the trained agent for further processing.")
+                write_results(
+                    # TODO: How to access the trained agent?
+                    self.global_settings.result_file(".pickle"),
+                )
