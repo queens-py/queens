f457f566bb941a0600335d9111ba6ae86329d64a
Daniel Wolff <d.wolff@unibw.de>
2025-02-07 10:15:12 +0100

    docs: finalize comments in rl_iterator class

--- queens/iterators/rl_iterator.py
+++ queens/iterators/rl_iterator.py
@@ -62,6 +62,9 @@ class RLIterator(Iterator):
         Args:
             model (RLModel): Model to be evaluated by the iterator.
             parameters (Parameters): Parameters object.
+                .. note::
+                        This parameter is required by the base class, but is
+                        currently not used in the RLIterator.
             global_settings (GlobalSettings): Settings of the QUEENS experiment including its name
                 and the output directory.
             result_description (dict): Description of desired results.
@@ -80,27 +83,32 @@ class RLIterator(Iterator):
         super().__init__(model, parameters, global_settings)
 
         self.result_description = result_description
-
-        if mode not in ["training", "evaluation"]:
-            raise ValueError(
-                f"Unsupported mode: {mode}\n" "The mode must be either `training` or `evaluation`."
-            )
-        self._mode = mode
-        self._interaction_steps = interaction_steps
-        self._initial_observation = initial_observation
+        self.mode = mode
+        self.interaction_steps = interaction_steps
+        self.initial_observation = initial_observation
 
         # Create a dictionary with empty lists to store the interaction data
         self.output = {"step": [], "result": []}
 
     @property
     def mode(self):
-        """str: Mode of the RLIterator."""
+        """Access the mode of the RLIterator.
+
+        Returns:
+            str: Mode of the RLIterator.
+        """
         return self._mode
 
     @mode.setter
     def mode(self, mode):
-        """Set the mode of the RLIterator."""
-        if not mode in ["training", "evaluation"]:
+        """Set the mode of the RLIterator.
+
+        Perform sanity checks to ensure that mode has a valid value.
+
+        Args:
+            mode (str): Mode of the RLIterator.
+        """
+        if mode not in ["training", "evaluation"]:
             raise ValueError(
                 f"Unsupported mode: {mode}\n" "The mode must be either `training` or `evaluation`."
             )
@@ -108,12 +116,23 @@ class RLIterator(Iterator):
 
     @property
     def interaction_steps(self):
-        """int: Number of interaction steps to be performed."""
+        """Access the number of interaction steps.
+
+        Returns:
+            int: Number of interaction steps to be performed.
+        """
         return self._interaction_steps
 
     @interaction_steps.setter
     def interaction_steps(self, eval_steps):
-        """Set the number of interaction steps."""
+        """Set the number of interaction steps.
+
+        Perform sanity checks to ensure that the number of interaction steps
+        has a valid value.
+
+        Args:
+            eval_steps (int): Number of interaction steps to be performed.
+        """
         if eval_steps < 0:
             raise ValueError(
                 f"Unsupported number of interaction steps: {eval_steps}\n"
@@ -121,22 +140,16 @@ class RLIterator(Iterator):
             )
         self._interaction_steps = eval_steps
 
-    @property
-    def initial_observation(self):
-        """np.ndarray: Initial observation of the environment."""
-        return self._initial_observation
-
-    @initial_observation.setter
-    def initial_observation(self, initial_observation):
-        """Set the initial observation of the environment."""
-        self._initial_observation = initial_observation
-
     def pre_run(self):
-        """Pre run portion of RLIterator."""
+        """Prepare the core run of the RLIterator (not needed here)."""
         _logger.info("Initialize RLIterator.")
 
     def core_run(self):
-        """Core run of RLIterator."""
+        """Core run of RLIterator.
+
+        Depending on the :py:attr:`mode` of the RLIterator, the agent is either
+        trained or used for evaluation.
+        """
         _logger.info("Welcome to Reinforcement Learning core run.")
         if self._mode == "training":
             _logger.info("Starting agent training.")
@@ -147,7 +160,7 @@ class RLIterator(Iterator):
             _logger.info("Agent training took %E seconds.", end - start)
         else:  # self._mode == 'evaluation'
             _logger.info("Starting interaction loop.")
-            if self._initial_observation is None:
+            if self.initial_observation is None:
                 _logger.debug(
                     "No initial observation provided.\n"
                     "Resetting environment to generate an initial observation."
@@ -155,7 +168,7 @@ class RLIterator(Iterator):
                 obs = self.model.get_initial_observation()
             else:  # initial observation has been provided by the user
                 _logger.debug("Using provided initial observation.")
-                obs = self._initial_observation
+                obs = self.initial_observation
             start = time.time()
             # Perform as many interaction steps as set by the user
             for step in range(self._interaction_steps):
@@ -169,7 +182,12 @@ class RLIterator(Iterator):
             _logger.info("Interaction loop took %E seconds.", end - start)
 
     def post_run(self):
-        """Post run portion of RLIterator."""
+        """Optionally export the results of the core run depending on the mode.
+
+        If the mode is set to 'training', the trained agent is stored
+        for further processing. If the mode is set to 'evaluation', the
+        interaction outputs are stored for further processing.
+        """
         if self.result_description:
             if self.result_description["write_results"]:
                 if self._mode == "training":
