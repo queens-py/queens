a7e3f96e7be5ce7cf2d2a44e51716453be21344a
Lea Häusel <lea.haeusel@tum.de>
2025-12-16 09:50:46 +0100

    docs: add remote computing tutorial

---
+++ tests/tutorial_tests/test_3_grid_iterator_fourc_remote.py
@@ -0,0 +1,19 @@
+#
+# SPDX-License-Identifier: LGPL-3.0-or-later
+# Copyright (c) 2024-2025, QUEENS contributors.
+#
+# This file is part of QUEENS.
+#
+# QUEENS is free software: you can redistribute it and/or modify it under the terms of the GNU
+# Lesser General Public License as published by the Free Software Foundation, either version 3 of
+# the License, or (at your option) any later version. QUEENS is distributed in the hope that it will
+# be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details. You
+# should have received a copy of the GNU Lesser General Public License along with QUEENS. If not,
+# see <https://www.gnu.org/licenses/>.
+#
+"""Placeholder for integration test of 3-grid-iterator-4C-remote tutorial.
+
+The actual test is in tests/integration_tests/cluster/test_dask_cluster.py.
+This file ensures that the tutorial is not run by tests/tutorial_tests/test_tutorials.py.
+"""
